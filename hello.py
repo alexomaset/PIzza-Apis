91f71f3d6d7a6f8f933e2108b10eaba065eaa371
Alex omaset <alexomaset711@gmail.com>
2020-05-18 11:39:32 +0300

    add initial changes

--- hello.py
+++ hello.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
+
+#Init app
 app = Flask(__name__)
 
 app.route('/')
 def index():
     return '<h1> Hello World <h1>'
-
+    
+# Run server
     if __name__ == '__main__':
          app.run(debug = True)
  
